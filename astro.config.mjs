0d2bab1b9d87a7c0a992037739d7e7f45584c928
Ihor Pysarenko <pysarenkoigorwot@gmail.com>
2026-04-14 22:38:47 +0300

    fix sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://ttnflow.com',
   integrations: [
     tailwind({ configFile: './tailwind.config.cjs' }),
     vue(),
-    sitemap(),
   ],
   output: 'static',
 });
