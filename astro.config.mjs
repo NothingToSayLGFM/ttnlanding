2e60387d58abb34ce030b995f5d96da8a992cb69
Ihor Pysarenko <pysarenkoigorwot@gmail.com>
2026-04-14 22:31:42 +0300

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://ttnflow.com',
   integrations: [
     tailwind({ configFile: './tailwind.config.cjs' }),
     vue(),
+    sitemap(),
   ],
   output: 'static',
 });
